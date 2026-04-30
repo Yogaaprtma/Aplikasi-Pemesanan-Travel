ec01de8d8199d0341ad75f1f2a2027da20e225c9
Yogaaprtma <yogapratama010503@gmail.com>
2026-04-30 08:09:11 +0700

    update TravelSchedule.php

--- app/Models/TravelSchedule.php
+++ app/Models/TravelSchedule.php
@@ -10,14 +10,48 @@ class TravelSchedule extends Model
     use HasFactory;
 
     protected $fillable = [
+        'origin',
         'destination',
         'departure_time',
         'quota',
-        'price'
+        'price',
+        'vehicle_type',
+        'description',
+    ];
+
+    protected $casts = [
+        'departure_time' => 'datetime',
+        'price'          => 'decimal:2',
     ];
 
     public function bookings()
     {
         return $this->hasMany(Booking::class);
+    }
+
+    /**
+     * Label kendaraan
+     */
+    public function getVehicleLabelAttribute(): string
+    {
+        return match ($this->vehicle_type) {
+            'bus'     => 'Bus',
+            'minivan' => 'Minivan',
+            'car'     => 'Mobil',
+            default   => 'Bus',
+        };
+    }
+
+    /**
+     * Icon kendaraan (Font Awesome class)
+     */
+    public function getVehicleIconAttribute(): string
+    {
+        return match ($this->vehicle_type) {
+            'bus'     => 'fa-bus',
+            'minivan' => 'fa-shuttle-van',
+            'car'     => 'fa-car',
+            default   => 'fa-bus',
+        };
     }
 }
